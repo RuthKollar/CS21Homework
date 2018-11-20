6fd2c99f96302292c295bf8d22a0dbb94f946369
Ruth <kollar.ruth@gmail.com>
2018-11-20 16:11:19 -0500

    Revert "Word Frequency Counter"
    This reverts commit b1304b919fe76ed40d7243c1e429641f10af39d2.

--- about_me.py
+++
@@ -1,9 +0,0 @@
-#Ruth Kollar
-#CS21 Section C
-#Assignment 1
-
-print("Ruth Kollar")
-print("I am a third year Math major")
-print("I am from Pittsburgh, Pennsylvania")
-print("I am an only child")
-print("I love to draw")
